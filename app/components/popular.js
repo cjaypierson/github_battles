089686325d5f98911d200b2c9f7051746755a6d4
Cameron Pierson <cameron.pierson@ubsh.corp>
2018-03-30 13:53:07 -0400

    added api call to github for repos

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 class SelectLanguage extends React.Component {
 	render() {
@@ -21,7 +22,7 @@ class SelectLanguage extends React.Component {
 	}
 }
 
-SelectLanguage.PropTypes = {
+SelectLanguage.propTypes = {
 	selectedLanguage: PropTypes.string.isRequired,
 	onSelect: PropTypes.func.isRequired
 }
@@ -30,17 +31,31 @@ class Popular extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		};
 
 		this.updateLanguage = this.updateLanguage.bind(this);
 	}
+	componentDidMount () {
+		this.updateLanguage(this.state.selectedLanguage);
+	};
 	updateLanguage(lang) {
 		this.setState(function () {
 			return {
-				selectedLanguage: lang
+				selectedLanguage: lang,
+				repos: null
 			}
 		});
+
+		api.fetchPopularRepos(lang)
+			.then(function (repos) {
+				this.setState(function () {
+					return {
+						repos: repos
+					}
+				})
+			}.bind(this));
 	};
 	render() {
 		return (
@@ -49,6 +64,7 @@ class Popular extends React.Component {
 				selectedLanguage = {this.state.selectedLanguage}
 				onSelect = {this.updateLanguage}
 				/>
+				{JSON.stringify(this.state.repos, null, 2)}
 			</div>
 		)
 	}
