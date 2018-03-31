c2b32d29f65657c7427c547e4e56588358645b0c
Cameron <cam@MacBook-Pro.fios-router.home>
2018-03-31 13:58:54 -0400

    built initial battle ui

--- app/components/popular.js
+++ app/components/popular.js
@@ -46,11 +46,11 @@ function RepoGrid (props) {
 	)
 }	
 
-RepoGrid.PropTypes = {
+RepoGrid.propTypes = {
 	repos: PropTypes.array.isRequired
 }
 
-SelectLanguage.PropTypes = {
+SelectLanguage.propTypes = {
 	selectedLanguage: PropTypes.string.isRequired,
 	onSelect: PropTypes.func.isRequired
 }
